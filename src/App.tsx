bfafefccb5b4bbdf591814d691850ed91811ff31
Gabriela Mapa <Gabriela.Mapa@phoenix.edu>
2026-05-08 11:39:37 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import GitHubActivity from './components/GitHubActivity';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
